af266ed8998ad11155da93c0c180f0cb0c45891c
poglodek <pawelogll@gmail.com>
2024-06-10 20:12:03 +0200

    File scope

--- src/FleetSoft/Application/Modules/Vehicle/Vehicle.Shared/VehicleModule.cs
+++ src/FleetSoft/Application/Modules/Vehicle/Vehicle.Shared/VehicleModule.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Vehicle.Shared;
 
-public class VehicleModule : IModule
+file class VehicleModule : IModule
 {
     public string ModuleName => "Vehicle";
     public IServiceCollection InstallModule(IServiceCollection services)
