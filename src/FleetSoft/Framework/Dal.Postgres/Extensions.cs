0ed3e29216b5343d781da45d8adc6ee0b73ce21d
poglodek <pawelogll@gmail.com>
2024-06-10 19:36:33 +0200

    Dal

---
+++ src/FleetSoft/Framework/Dal.Postgres/Extensions.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dal.Postgres;
+
+public static class Extensions
+{
+    public static WebApplicationBuilder AddDatabase<T>(this WebApplicationBuilder builder, string connectionStringName) where T: DbContext
+    {
+        var connectionString = builder.Configuration.GetConnectionString(connectionStringName);
+        
+        builder.Services.AddDbContext<T>(x =>
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionStringName));
+            }
+
+            x.UseNpgsql(connectionString);
+        });
+
+
+        return builder;
+    }
+
+    public static WebApplication UseMigration<T>(this WebApplication application) where T: DbContext
+    {
+        using var scope = application.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<T>();
+        
+        db.Database.Migrate();
+        
+        return application;
+    }
+}
