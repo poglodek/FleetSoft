6088e055d84b455bad3b05d614bb22613d758d7e
poglodek <pawelogll@gmail.com>
2024-07-28 17:43:18 +0200

    API moudle - vehicle

--- src/FleetSoft/Application/Modules/Vehicle/Vehicle.Shared/VehicleModule.cs
+++ src/FleetSoft/Application/Modules/Vehicle/Vehicle.Shared/VehicleModule.cs
@@ -1,11 +1,16 @@
 using ApiShared;
-using Dal.Postgres;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Vehicle.Application.Command.AddMileage;
+using Vehicle.Application.Command.CreateVehicle;
+using Vehicle.Application.Command.SetArchive;
 using Vehicle.Infrastructure;
-using Vehicle.Infrastructure.Database;
+using Vehicle.Infrastructure.Query.GetById;
 
 namespace Vehicle.Shared;
 
@@ -21,7 +26,33 @@ file class VehicleModule : IModule
 
     public IEndpointRouteBuilder AddEndPoints(IEndpointRouteBuilder endpointRoute)
     {
+        endpointRoute.MapGet("/{id}", async (Guid id, IMediator mediator) =>
+        {
+            var result = await mediator.Send(new GetByIdRequest(id));
+
+            return Results.Ok(result);
+        });
         
+        endpointRoute.MapPost("/", async (CreateVehicleRequest request, IMediator mediator) =>
+        {
+            var result = await mediator.Send(request);
+
+            return Results.Ok(result);
+        });
+        
+        endpointRoute.MapPut("/{id}/Archive", async (Guid id, IMediator mediator) =>
+        {
+            var result = await mediator.Send(new SetArchiveRequest(id));
+
+            return Results.Ok(result);
+        });
+        
+        endpointRoute.MapPut("/{id}/Mileage", async (Guid id,[FromQuery]double value, IMediator mediator) =>
+        {
+            var result = await mediator.Send(new AddMileageRequest(id,value));
+
+            return Results.Ok(result);
+        });
 
         return endpointRoute;
     }
